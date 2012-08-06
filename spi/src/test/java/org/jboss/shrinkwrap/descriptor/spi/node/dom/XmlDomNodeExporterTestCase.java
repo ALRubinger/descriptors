f58950669e3137771874736f9eb0e38f10161c9d
Andrew Lee Rubinger <alr@alrubinger.com>
2012-08-06 03:47:54 -0400

    [SHRINKDESC-125] Remove artifacts left over from copy/paste

--- spi/src/test/java/org/jboss/shrinkwrap/descriptor/spi/node/dom/XmlDomNodeExporterTestCase.java
+++ spi/src/test/java/org/jboss/shrinkwrap/descriptor/spi/node/dom/XmlDomNodeExporterTestCase.java
@@ -10,7 +10,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,150 +21,92 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 import org.jboss.shrinkwrap.descriptor.api.DescriptorExportException;
-import org.jboss.shrinkwrap.descriptor.spi.node.DummyNodeDescriptor;
 import org.jboss.shrinkwrap.descriptor.spi.node.Node;
 import org.jboss.shrinkwrap.descriptor.test.util.XmlAssert;
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * TestCases to see Exporter can handle the Node Structure provided by the Importer.
- * 
+ *
  * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
  * @author <a href="mailto:ralf.battenfeld@bluewin.ch">Ralf Battenfeld</a>
- * @version $Revision: $
+ * @author <a href="mailto:alr@jboss.org">Andrew Lee Rubinger</a>
  */
-public class XmlDomNodeExporterTestCase
-{
-   private static final String XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>";
-   
-   private static final String XML_WITH_COMMENT = "" +
-   		XML_HEADER +
-   		"<root>" +
-   		"  <!-- comment -->" +
-   		"  <child>test</child>" +
-   		"</root>"; 
+public class XmlDomNodeExporterTestCase {
+    private static final String XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>";
 
-   /*
-    * SHRINKDESC-31 - Comments in XML input Cause Export error
-    * 
-    * Node[#comment] children[0] text[ comment ] 
-    * Caused by: org.w3c.dom.DOMException: INVALID_CHARACTER_ERR: An invalid or illegal XML character is specified. 
-    *  
-    */
-   @Test
-   public void shouldBeAbleToImportXMLWithComments() throws Exception
-   {
-      Node root = load(XML_WITH_COMMENT);
-      
-      Assert.assertNotNull("Obtaining comment should not be null",root.getSingle("#comment"));
-      Assert.assertNotNull("Obtaining child should not be null",root.getSingle("child"));
-   }
-   
-   /*
-    * SHRINKDESC-31 - Comments in XML input Cause Export error
-    * 
-    * Node[#comment] children[0] text[ comment ] 
-    * Caused by: org.w3c.dom.DOMException: INVALID_CHARACTER_ERR: An invalid or illegal XML character is specified. 
-    *  
-    */
-   @Test
-   public void shouldBeAbleExportXMLWithComments() throws Exception
-   {
-      // given
-      Node root = load(XML_WITH_COMMENT);
-      
-      // when
-      String exportedXml = exportAsString(root);
-      
-      // then
-      XmlAssert.assertIdentical(XML_WITH_COMMENT, exportedXml);
-   }
-   
-   @Test
-   public void shouldBeAbleToExportSingleNodeTree() throws Exception
-   {
-      // given
-      Node root = new Node("root");
+    private static final String XML_WITH_COMMENT = "" + XML_HEADER + "<root>" + "  <!-- comment -->"
+        + "  <child>test</child>" + "</root>";
 
-      // when
-      String exportedXml = exportAsString(root);
+    /**
+     * Test export counterpoint to SHRINKDESC-31
+     */
+    @Test
+    public void shouldBeAbleExportXMLWithComments() throws Exception {
+        // given
+        Node root = load(XML_WITH_COMMENT);
 
-      // then
-      XmlAssert.assertIdentical(XML_HEADER + "<root></root>", exportedXml);
-   }
-   
-   @Test
-   public void shouldBeAbleToExportSingleNodeTreeWithAttributes() throws Exception
-   {
-      // given
-      Node root = new Node("root");
-      root.attribute("id", "1").attribute("name", "root");
+        // when
+        String exportedXml = exportAsString(root);
 
-      // when
-      String exportedXml = exportAsString(root);
+        // then
+        XmlAssert.assertIdentical(XML_WITH_COMMENT, exportedXml);
+    }
 
-      // then
-      XmlAssert.assertIdentical(XML_HEADER + "<root id=\"1\" name=\"root\"></root>", exportedXml);
+    @Test
+    public void shouldBeAbleToExportSingleNodeTree() throws Exception {
+        // given
+        Node root = new Node("root");
 
-   }
-   
-   @Test
-   public void shouldBeAbleToExportSingleNodeTreeWithAttributesAndText() throws Exception
-   {
-      // given
-      Node root = new Node("root");
-      root.attribute("id", "1")
-          .attribute("name", "root")
-          .text("doovde");
+        // when
+        String exportedXml = exportAsString(root);
 
-      // when
-      String exportedXml = exportAsString(root);
-      
-      // then
-      XmlAssert.assertIdentical(XML_HEADER + "<root id=\"1\" name=\"root\">doovde</root>", exportedXml);
+        // then
+        XmlAssert.assertIdentical(XML_HEADER + "<root></root>", exportedXml);
+    }
 
-   }
-   
-   @Test
-   public void shouldCreateSingleNodeWithGivenNameWhenEmptyStringPassedToImport() throws Exception
-   {
-      // given
-      XmlDomNodeDescriptorImporterImpl<DummyNodeDescriptor> xmlDomNodeDescriptorImporter = new XmlDomNodeDescriptorImporterImpl<DummyNodeDescriptor>(DummyNodeDescriptor.class, "test-descriptor");
-      Node expectedRoot = new Node("dummy");
-      
-      // when
-      Node importedRoot = xmlDomNodeDescriptorImporter.fromString("").getRootNode();
+    @Test
+    public void shouldBeAbleToExportSingleNodeTreeWithAttributes() throws Exception {
+        // given
+        Node root = new Node("root");
+        root.attribute("id", "1").attribute("name", "root");
 
-      // then
-      Assert.assertEquals(expectedRoot.getName(), importedRoot.getName());
-      Assert.assertTrue(importedRoot.getChildren().isEmpty());
-   }
+        // when
+        String exportedXml = exportAsString(root);
 
-   
-   @Test(expected = DescriptorExportException.class)
-   public void shouldThrowExceptionWhenTreeIsNull() throws Exception
-   {
-      // given
-      Node root = null;
+        // then
+        XmlAssert.assertIdentical(XML_HEADER + "<root id=\"1\" name=\"root\"></root>", exportedXml);
 
-      // when
-      String exportedXml = exportAsString(root);
+    }
 
-      // then
-      // exception should be thrown
-   }
-   
-   // Private utility methods
-   
-   private Node load(String xml)
-   {
-      return XmlDomNodeImporter.INSTANCE.importAsNode(new ByteArrayInputStream(xml.getBytes()), true);
-   }
-   
-	private String exportAsString(Node root) {
-		final ByteArrayOutputStream output = new ByteArrayOutputStream();
-		XmlDomNodeExporter.INSTANCE.to(root, output);
-		return new String(output.toByteArray());
-	}
+    @Test
+    public void shouldBeAbleToExportSingleNodeTreeWithAttributesAndText() throws Exception {
+        // given
+        Node root = new Node("root");
+        root.attribute("id", "1").attribute("name", "root").text("doovde");
+
+        // when
+        String exportedXml = exportAsString(root);
+
+        // then
+        XmlAssert.assertIdentical(XML_HEADER + "<root id=\"1\" name=\"root\">doovde</root>", exportedXml);
+
+    }
+
+    @Test(expected = DescriptorExportException.class)
+    public void shouldThrowExceptionWhenTreeIsNull() throws Exception {
+        exportAsString(null);
+    }
+
+    // Private utility methods
+
+    private Node load(String xml) {
+        return XmlDomNodeImporter.INSTANCE.importAsNode(new ByteArrayInputStream(xml.getBytes()), true);
+    }
+
+    private String exportAsString(Node root) {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        XmlDomNodeExporter.INSTANCE.to(root, output);
+        return new String(output.toByteArray());
+    }
 }
